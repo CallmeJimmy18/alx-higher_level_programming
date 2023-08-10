264b831ebc7910cfe7aa37420e7e0df7795d6ffa
Garnet Matola <matolagarnet.com>
2023-08-10 09:47:57 -0700

    changed logic

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,7 +8,9 @@ if __name__ == "__main__":
 
     if arg_count == 0:
         print("{} arguments.".format(arg_count))
+    elif arg_count == 1:
+        print("{} argument:".format(arg_count))
     else:
         print("{} arguments:".format(arg_count))
-        for i in range(1, arg_count + 1):
-            print("{}: {}".format(i, argv[i]))
+    for i in range(1, arg_count + 1):
+        print("{}: {}".format(i, argv[i]))
