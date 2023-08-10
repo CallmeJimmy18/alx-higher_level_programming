d608ee72ab171cbabc1d996ef973fa002fdeaea6
Garnet Matola <matolagarnet.com>
2023-08-10 09:19:27 -0700

    no import

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-import sys
-arg_count = 0
-for argc in range(0, len(sys.argv)):
-    if argc != 0:
-        arg_count += 1
-if arg_count == 0:
-    print("{} arguments.".format(arg_count))
-else:
-    print("{} arguments:".format(arg_count))
-    for i in range(1, arg_count + 1):
-        print("{}: {}".format(i, sys.argv[i]))
+if __name == "__main__":
+    import sys
+    arg_count = 0
+    for argc in range(0, len(sys.argv)):
+        if argc != 0:
+            arg_count += 1
+
+    if arg_count == 0:
+        print("{} arguments.".format(arg_count))
+    else:
+        print("{} arguments:".format(arg_count))
+        for i in range(1, arg_count + 1):
+            print("{}: {}".format(i, sys.argv[i]))
