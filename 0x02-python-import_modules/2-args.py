151ad73cd3d378698cc7762416d13234988ccb20
Garnet Matola <matolagarnet.com>
2023-08-10 09:35:16 -0700

    Change import

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name == "__main__":
-    import sys
+    from sys import argv
     arg_count = 0
-    for argc in range(0, len(sys.argv)):
+    for argc in range(0, len(argv)):
         if argc != 0:
             arg_count += 1
 
@@ -11,4 +11,4 @@ if __name == "__main__":
     else:
         print("{} arguments:".format(arg_count))
         for i in range(1, arg_count + 1):
-            print("{}: {}".format(i, sys.argv[i]))
+            print("{}: {}".format(i, argv[i]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
+    from sys import argv
     add = 0
 
-    for i in range(1, len(sys.argv)):
-        add += int(sys.argv[i])
+    for i in range(1, len(argv)):
+        add += int(argv[i])
     print("{}".format(add))
