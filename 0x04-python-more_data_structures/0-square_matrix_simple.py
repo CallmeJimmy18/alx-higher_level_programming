cd9afa18487a15df82ccc99769af82b7b6863121
Garnet Matola <matolagarnet.com>
2023-08-16 16:40:46 -0700

    touch ups

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     squared = matrix.copy()
     for i in range(len(matrix)):
-        squared[i] = list(map((lambda x: x ** 2), matrix[i]))
+        squared[i] = list(map(lambda x: x ** 2, matrix[i]))
     return squared
