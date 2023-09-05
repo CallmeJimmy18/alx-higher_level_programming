11b7a8a74b5f6514a06697a346f4cfda0875c4f5
Garnet Matola <matolagarnet.com>
2023-09-05 13:01:47 -0700

    done a few corrections

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-"""Define function that returns copy of list"""
-
-
 def copy_list(l):
-    """A copy of list l will be store in cpy_l"""
-    cpy_l = l[:]
-
-    return cpy_l
+    return l[:]
