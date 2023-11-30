748f6428c5bad9f09a6a9961363f55e9404db864
Garnet Matola <matolagarnet@gmail.com>
2023-11-30 11:21:31 -0800

    debugging 4 and 5

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a GET request to the URL, and displays the body
-x_school_user_id=98; curl -X GET -H "X-School-User-Id: $x_school_user_id" "$1"
+curl -X GET -H "X-School-User-Id: 98" "$1"
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a POST request to the passed URL, and displays the body
-val1="test@gmail.com"; val2="I will always be here for PLD"; curl -X POST -d "email=$val1&subject=$val2" "$1"
+curl -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
